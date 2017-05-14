c5853db0d8bba2b880a1776e9272dd4a35d66b44
JohnAntonioCollins <john@johncollins.ws>
2017-05-14 13:23:00 -0400

    first commit

---
+++ src/main/java/Solution.java
@@ -0,0 +1,39 @@
+/**
+ * @Author John Antonio Collins
+ * Created on 5/14/17.
+ */
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    static int[] solve(int a0, int a1, int a2, int b0, int b1, int b2){
+        // Complete this function
+        //MY CODE START
+        //PLAN: int[2], for-loop, comparison, add points, update var each iteration.
+        int[] solution = new int[2];
+        if(a0)
+        return solution;
+        //MY CODE END
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int a0 = in.nextInt();
+        int a1 = in.nextInt();
+        int a2 = in.nextInt();
+        int b0 = in.nextInt();
+        int b1 = in.nextInt();
+        int b2 = in.nextInt();
+        int[] result = solve(a0, a1, a2, b0, b1, b2);
+        for (int i = 0; i < result.length; i++) {
+            System.out.print(result[i] + (i != result.length - 1 ? " " : ""));
+        }
+        System.out.println("");
+
+
+    }
+}
